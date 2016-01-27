94d21428cf328468f68f7ba0b8c366d0f30d4ffd
Sergey <sergio_lebedev@yahoo.it>
2016-01-27 15:40:50 +0100

    release first sprint

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
+$(document).ready(function(){
+    
+    $("#checkBtn").click(function(){
+           console.log( $("#inputPhrase").val() );
+            FlirtrController.check();
+            console.log( $("#inputPhrase").val().search(patterns[0].word) );
+            console.log( $("#inputPhrase").val().search(patterns[0].neutraliser) );
+       });
+    
+    
+    
+});
+
 var FlirtrModel = {  
+    
+    score: 0,
+    
+    /* initialise model */
+    init : function(){
+        this.score = 0;
+        $(".answer").hide();
+    },
   
    /* It sets the phrase to analyse */
    analyse : function (phrase){
+       
+       this.init();
+       
+       for (var i; i< patterns.length; i++){
+           
+           if(phrase.search(patterns[i].word) =! -1){             
+            
+                if(phrase.search(patterns[i].neutraliser) == -1){
+                    this.score = parseInt(this.score + patterns[i].score); 
+                }
+           }
+       }
+       
    },
   
    /* Returns the flirting score (number) of the phrase.
@@ -11,6 +45,33 @@ var FlirtrModel = {
     * the right of the matching word)
     */
    getScore : function () {
+       
+       FlirtrView.update(this.score);
+       return this.score;
    }
          
 };
+
+var FlirtrView = {
+    
+    /* view takes care of updating HTML document with current score */
+    update: function(score){
+        
+        $(".answer").text("Score: "+score);
+        $(".answer").show();
+        
+    }
+    
+    
+    
+};
+
+var FlirtrController = {
+    
+    check: function() {
+        
+        FlirtrModel.analyse( $("#inputPhrase").val() );
+        FlirtrModel.getScore();
+    }
+    
+}
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -3,8 +3,29 @@
 
 describe("Flirtr", function() {
 
-  it("should...", function() {  
+  it("should not be adding score with wrong input", function() {  
     
-  });    
+      FlirtrModel.analyse("aaa");
+      expect(FlirtrModel.getScore()).toBe(0);
+      
+  });
+    
+    it("should be adding score with a right input with no neutralisers"), function(){
+        
+        FlirtrModel.analyse("asdnasd love asd");
+      expect(FlirtrModel.getScore()).toBe(1);
+    }
+    
+    it("should not be adding score with a right input when there's also a neutraliser"), function(){
+        
+        FlirtrModel.analyse("asd friend nasd love asd");
+      expect(FlirtrModel.getScore()).toBe(0);
+    }
+    
+    it("should correctly be adding more points when more results hit with no neutraliser"), function(){
+        
+        FlirtrModel.analyse("asdnasd love asd kiss");
+      expect(FlirtrModel.getScore()).toBe(2);
+    }
  
 });
